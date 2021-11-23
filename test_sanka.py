0c3bdccfd818ad18fa9dc2df63c36d376bbfcdcb
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-11-23 02:45:38 -0600

    adds another test

--- test_sanka.py
+++ test_sanka.py
@@ -5,7 +5,7 @@ from sanka import Sanka
 
 class SankaTest(TestCase):
 
-    def test_can_count_function_calls(self):
+    def test_count_calls_for_no_arg_function(self):
 
         @Sanka
         def function():
@@ -15,6 +15,19 @@ class SankaTest(TestCase):
 
         for i in range(num_calls):
             function()
+
+        assert function('ya dead?') == num_calls
+
+    def test_count_calls_for_function_with_args(self):
+
+        @Sanka
+        def function(takes, some_args):
+            pass
+
+        num_calls = 10
+
+        for i in range(num_calls):
+            function('takes', 'some_args')
 
         assert function('ya dead?') == num_calls
 
