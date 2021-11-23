b1d9581229afdb551ab810280f0149afad628a87
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-11-23 02:49:33 -0600

    adds Sanka.YaDead

--- sanka.py
+++ sanka.py
@@ -6,8 +6,11 @@ class Sanka:
 
     def __call__(self, *args, **kwargs):
         # TODO (withtwoemms) -- match type instead of string
-        if 'ya dead?' in args:
+        if Sanka.YaDead in args:
             return self.call_count
         self.call_count += 1
         return self.func(*args, **kwargs)
+
+    class YaDead:
+        pass
 
