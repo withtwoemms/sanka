3c2306edb463b4ed2d916840b1e7454318ebeaf2
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-11-26 02:43:28 -0600

    un-nests Sanka.YaDead

--- sanka.py
+++ sanka.py
@@ -5,12 +5,12 @@ class Sanka:
         self.call_count = 0
 
     def __call__(self, *args, **kwargs):
-        # TODO (withtwoemms) -- match type instead of string
-        if Sanka.YaDead in args:
+        if YaDead in args:
             return self.call_count
         self.call_count += 1
         return self.func(*args, **kwargs)
 
-    class YaDead:
-        pass
+
+class YaDead:
+    pass
 
