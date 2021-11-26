f75a0425e57f5ba6d7134b16d906f73d37d6aa04
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-11-26 02:48:42 -0600

    adds tests verifying Sanka can monitor for multiple functions

--- test_sanka.py
+++ test_sanka.py
@@ -12,7 +12,7 @@ class SankaTest(TestCase):
         def function():
             pass
 
-        num_calls = 10
+        num_calls = 2
 
         for i in range(num_calls):
             function()
@@ -25,10 +25,30 @@ class SankaTest(TestCase):
         def function(takes, some_args):
             pass
 
-        num_calls = 10
+        num_calls = 2
 
         for i in range(num_calls):
             function('takes', 'some_args')
 
         assert function(YaDead) == num_calls
+
+    def test_count_calls_for_multiple_functions(self):
+
+        @Sanka
+        def function1():
+            pass
+        @Sanka
+        def function2():
+            pass
+
+        num_calls_for_first_function = 2
+        num_calls_for_second_function = 3
+
+        for i in range(num_calls_for_first_function):
+            function1()
+        for i in range(num_calls_for_second_function):
+            function2()
+
+        assert function1(YaDead) == num_calls_for_first_function
+        assert function2(YaDead) == num_calls_for_second_function
 
