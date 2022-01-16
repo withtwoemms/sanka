6cc61ea9f95b8e1fc999b08562a741c7a78d29c4
Emmanuel I. Obi <withtwoemms@gmail.com>
2022-01-16 13:34:39 -0600

    adds "image" session to noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
 import nox
 
 from os import environ as envvar
+from subprocess import check_output
+from subprocess import Popen
+from subprocess import PIPE
 
 
 PROJECT_NAME = 'sanka'
@@ -22,6 +25,22 @@ nox.options.default_venv_backend = 'none' if not USEVENV else USEVENV
 
 def session_name(suffix: str):
     return f'{VENV}-{suffix}' if USEVENV else suffix
+
+
+def project_version():
+    tags = Popen('git tag'.split(), stdout=PIPE)
+    output = check_output('tail -1'.split(), stdin=tags.stdout)
+    return output.decode().strip('\n')
+
+
+def image_name():
+    return f'{PROJECT_NAME}:{project_version()}'
+
+
+@nox.session(name=session_name('image'), python=supported_python_versions)
+def image(session):
+    command = f'docker build -t {image_name()} .'
+    session.run(*command.split(' '))
 
 
 @nox.session(name=session_name('install'), python=supported_python_versions)
@@ -58,7 +77,4 @@ def test(session):
 @nox.session(name=session_name('build'), python=supported_python_versions)
 def build(session):
     session.run('python', 'setup.py', 'sdist')
-
-
-# TODO (withtwoemms) -- leverage gitpython to show version on-demand
 
